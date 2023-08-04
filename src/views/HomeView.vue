4bec8c1d1f7a28bb893e0dccd14df703369e8771
BeyondNaxos <adopteuntrip@gmail.com>
2023-08-04 13:54:24 +0200

    Handle Enter State

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,84 @@
 <script setup>
-import {ref} from 'vue'
+import {ref  } from 'vue'
 
-const showGreet = ref(true)
+
+const names = ref([
+  'John Doe',
+  'Jane Doe',
+])
+
+const nameInput = ref('')
+
+const addInvitee = () => {
+  names.value.unshift(nameInput.value)
+  nameInput.value = ''
+}
+
 </script>
 
 <template>
   <main>
+
+    <div class="container">
+      <input 
+        v-model="nameInput"
+        type="text" 
+        placeholder="Add person to invite..." 
+        @keypress.enter="addInvitee" 
+      >
+      <ul>
+        <li v-for="name in names" :key="name">{{name}}</li>
+   
+      </ul>
+    </div>
+
+
+  </main>
+</template>
+
+
+<style scoped>
+
+main { 
+  width: 100%;
+}
+.container {
+  max-width: 300px;
+  margin: 0 auto;
+} 
+
+.container input {
+  width: 100%;
+  border-radius: 5px;
+  border: 1px solid rgba(128, 128, 128, 0.13);
+  padding: 10px;
+  margin-bottom: 20px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.12); 
+}
+
+ul {
+  display: inline;
+  list-style: noe;
+  margin: 0px;
+  padding: 0px;
+}
+
+li {
+  list-style: none;
+  width: 300px;
+  border-radius: 5px;
+  padding : 5px 10px;
+  margin-top: 10px;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.12); 
+  text-align: center; 
+  cursor: pointer;
+  
+}
+</style>
+
+
+<!-- <template>
+   <main>
       <div class="container">
         <Transition name="fade">
           <h1 v-if="showGreet">Hello World</h1>
@@ -13,10 +86,9 @@ const showGreet = ref(true)
         </Transition>
       </div>
       <button @click="showGreet = !showGreet">Toogle</button>
-  </main>
-</template>
-
-<style  scoped>
+  </main> 
+</template> -->
+<!-- <style  scoped>
 /* Début de l'animation entry */
 .fade-enter-from {
  opacity: 0;
@@ -57,8 +129,8 @@ h1 {
 
 button {
   margin-top: 50px;
-}
+} 
 
 
-</style>
+</style> -->
  
