05dbce3b47bd579488df6f3d4ea7af3ae66b5515
BeyondNaxos <adopteuntrip@gmail.com>
2023-08-04 12:43:38 +0200

    Animating Component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import {ref} from 'vue'
+
+const showText = ref(false)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+      <Transition name="color">
+        <h1 v-if="showText">Hello world</h1>
+      </Transition>
+      <button @click="showText = !showText">Toogle</button>
   </main>
 </template>
+
+<style  scoped>
+.color-enter-from {
+  background-color: indianred; 
+  color: white;
+}
+
+.color-enter-to {
+  background-color: lightsalmon; 
+  color: black; 
+}
+
+.color-enter-active {
+  transition: all 1s;
+} 
+
+
+</style>
+ 
